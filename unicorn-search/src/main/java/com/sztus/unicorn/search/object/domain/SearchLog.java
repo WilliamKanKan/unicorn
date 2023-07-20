2bb940ffbb33c619639a5ecb45cf879cf580876b
mengmeng0531 <dongchengren001@gmail.com>
2023-07-21 05:02:41 +0800

    json

--- unicorn-search/src/main/java/com/sztus/unicorn/search/object/domain/SearchLog.java
+++ unicorn-search/src/main/java/com/sztus/unicorn/search/object/domain/SearchLog.java
@@ -1,18 +1,23 @@
 package com.sztus.unicorn.search.object.domain;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 @TableName(value = "search_log")
+
 public class SearchLog implements Serializable {
     @TableId(type = IdType.AUTO)
     private Long id;
     private Long userId;
-    private String question;
+    private String searchAnswers;
+    private String searchQuestions;
     private Long createdAt;
 }
--- unicorn-search/src/main/java/com/sztus/unicorn/search/service/SearchLogService.java
+++ unicorn-search/src/main/java/com/sztus/unicorn/search/service/SearchLogService.java
@@ -5,11 +5,16 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sztus.unicorn.lib.core.enumerate.CodeEnum;
 import com.sztus.unicorn.lib.core.type.AjaxResult;
 import com.sztus.unicorn.lib.core.type.NumberCode;
+import com.sztus.unicorn.search.object.domain.SearchAnswer;
 import com.sztus.unicorn.search.object.domain.SearchLog;
+import com.sztus.unicorn.search.object.domain.SearchQuestion;
+import com.sztus.unicorn.search.object.response.AnswerResponse;
 import com.sztus.unicorn.search.repository.mapper.SearchLogMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,14 +23,21 @@ public class SearchLogService {
     @Autowired
     SearchLogMapper searchLogMapper;
     // 通过user_id查一个人的search记录
-    public List<SearchLog> queryQuestionByUserIdAndDate(Long userId, Long startDate, Long endDate) {
+    public List<SearchLog> queryQuestionByUserId(Long userId) {
         LambdaQueryWrapper<SearchLog> logLambdaQueryWrapper = new LambdaQueryWrapper<>();
         logLambdaQueryWrapper.eq(SearchLog::getUserId, userId);
-        logLambdaQueryWrapper.between(SearchLog::getCreatedAt, startDate, endDate);
         logLambdaQueryWrapper.orderByDesc(SearchLog::getCreatedAt);
         return searchLogMapper.selectList(logLambdaQueryWrapper);
     }
+    public JSONObject queryQuestionById(Long id){
+        LambdaQueryWrapper<SearchLog> logLambdaQueryWrapper = new LambdaQueryWrapper<>();
+        logLambdaQueryWrapper.eq(SearchLog::getId, id);
+        logLambdaQueryWrapper.orderByDesc(SearchLog::getCreatedAt);
+        SearchLog log = searchLogMapper.selectOne(logLambdaQueryWrapper);
+        return JSONObject.parseObject(AjaxResult.success(log, CodeEnum.SUCCESS.getText()));
 
+
+    }
     public JSONObject deleteLogs(Long id, Long userId) {
         LambdaQueryWrapper<SearchLog> logLambdaQueryWrapper = new LambdaQueryWrapper<>();
         if (id != null) {
@@ -35,9 +47,7 @@ public class SearchLogService {
         }
         int row = searchLogMapper.delete(logLambdaQueryWrapper);
         if (row > 0) {
-            JSONObject response = JSONObject.parseObject(AjaxResult.success(CodeEnum.SUCCESS.getText()));
-            response.remove("data");
-            return response;
+            return JSONObject.parseObject(AjaxResult.success());
         } else {
             return JSON.parseObject(AjaxResult.failure(CodeEnum.FAILURE.getText()));
         }
